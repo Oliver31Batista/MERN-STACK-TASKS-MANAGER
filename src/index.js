3092175669f35be3b67d0ad8eea57dd89ae02a52
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-08-10 11:37:02 -0400

    initial structure

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use('/api/tasks' ,require('./routes/task.routes'))
+
+//static files
+app.use(express.static(path.join(__dirname, 'client')))
+
+//starting the server
+
+app.listen(app.get('port'), () => {
+    console.log(`server running on port: ${app.get('port')}`)
+})
